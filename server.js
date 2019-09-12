3865784ea70a0d10e92fe06ee9e620597d3b2287
dpham-p <donaldpham402@gmail.com>
2019-09-12 14:53:25 -0500

    Prep for deployment

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ if (process.env.NODE_ENV === 'production') {
   console.log('Development server running...');
   server = https.createServer(certOptions, app);
 }
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
